997427f5b701a4a104573a49225ea7092dde14c6
daren <darenhein@hotmail.com>
2025-03-01 21:30:55 -0600

    agregando documento js apra el login

---
+++ Template/duralux/html/crm/dist/assets/js/auth-login-cover.js
@@ -0,0 +1,12 @@
+// todo validaciones de los campos de login 
+// todo auth-login-cover.html 
+
+document.addEventListener('DOMContentLoaded', function() {
+    //* tomamos los campos de validaciones 
+    console.log("hola mundo")
+})
+
+function validateLogin() {
+
+
+}
