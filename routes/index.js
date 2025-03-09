99a35596f7dd35908da558c8ad53559c47e25601
daren <darenhein@hotmail.com>
2025-03-08 22:05:47 -0600

    creadno rutas

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const path = require('path');
 // Ruta principal "/"
 router.get('/', (req, res) => {
     // Proporcionar la ruta absoluta al archivo HTML en la carpeta "public"
-    const filePath = path.join(__dirname, '../public/auth-404-minimal.html');
-    res.sendFile(filePath);
+    const filePath = path.join(__dirname, '../public/auth-login-cover.html');
+    res.status(200).sendFile(filePath);
 });
 
 // Ruta "/login"
@@ -16,7 +16,8 @@ router.get('/login', (req, res) => {
 
 // Middleware para manejar rutas no encontradas
 router.use((req, res) => {
-    res.status(404).send('Error 404: Página no encontrada');
+    const filePath = path.join(__dirname, '../public/auth-404-minimal.html');
+    res.status(404).sendFile(filePath);
 });
 
 module.exports = router;
